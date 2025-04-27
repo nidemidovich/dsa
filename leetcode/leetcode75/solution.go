3cbb38fa237422a9b20846fe793dbf6852d3b89c
Nikita Demidovich <nidemidovich@yandex.ru>
2025-04-27 19:58:06 +0300

    leetcode 75: dutch national flag problem

---
+++ leetcode/leetcode75/solution.go
@@ -0,0 +1,20 @@
+package leetcode75
+
+// Dutch national flag problem
+func sortColors(nums []int) {
+	low, mid, high := 0, 0, len(nums)-1
+
+	for mid <= high {
+		switch nums[mid] {
+		case 0:
+			nums[low], nums[mid] = nums[mid], nums[low]
+			low++
+			mid++
+		case 1:
+			mid++
+		case 2:
+			nums[mid], nums[high] = nums[high], nums[mid]
+			high--
+		}
+	}
+}
